00539d4bd6aaade179fd3478c6f37736b566dd9a
Krishna Prasad Lenka <krishna.lenka@kaleidoxp.com>
2026-07-20 19:41:09 +0530

    new

---
+++ function_3.py
@@ -0,0 +1,19 @@
+#def print_hello():# function define kiya 
+ #   print ("hello")#ye batata hai function ka kaam kya hai 
+
+#print_hello()
+
+
+# write a function to find average of 3 numbers 
+
+def find_average(a,b,c):#defined a function
+    operation = (a+b+c)/3
+    return(operation)
+answer = find_average(5,5,10)
+print("the answer is ",answer)
+
+
+
+
+
+    
