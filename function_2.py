0eeb57e9d880a33a0eaba019d0f35baf8767c6df
Krishna Prasad Lenka <krishna.lenka@kaleidoxp.com>
2026-07-20 19:21:17 +0530

    first commit - functions practice

---
+++ function_2.py
@@ -0,0 +1,7 @@
+def result(a,b):
+    ans = a * b
+    return ans
+ 
+
+answer = result(21,2)
+print("the answer is", answer)
---
+++ functions_1.py
@@ -0,0 +1,7 @@
+#creatinga function to calculate addtion
+def sum_cal(n1 , n2):
+    sum = n1 + n2
+    print (sum)
+    return (sum)
+sum_cal(5,4)
+
